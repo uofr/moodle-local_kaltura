e94d57de4e7859131caa7db9038916461564caf5
Trevor Cunningham <trevor.cunningham@uregina.ca>
2020-08-26 09:44:12 -0600

    updates the kaltura_player to return an iframe embed

--- classes/kaltura_player.php
+++ classes/kaltura_player.php
@@ -74,34 +74,8 @@ class kaltura_player {
 
         $uid  = floor(microtime(true));
         $uid .= '_' . mt_rand();
-
-        $output = '';
-
-        $output .= "<object id=\"kaltura_player_{$uid}\" name=\"kaltura_player_{$uid}\" ";
-        $output .= "type=\"application/x-shockwave-flash\" allowFullScreen=\"true\" allowNetworking=\"all\" ";
-        $output .= "allowScriptAccess=\"always\"";
-        $output .= "xmlns:dc=\"http://purl.org/dc/terms/\" xmlns:media=\"http://search.yahoo.com/searchmonkey/media/\" ";
-        $output .= "rel=\"media:{$entryobj->mediaType}\" ";
-        $output .= "resource=\"{$host}/index.php/kwidget/wid/_{$entryobj->partnerId}/uiconf_id/{$uiconf}/entry_id/{$entryobj->id}\" ";
-        $output .= "data=\"{$host}/index.php/kwidget/wid/_{$entryobj->partnerId}/uiconf_id/{$uiconf}";
-        $output .= "/entry_id/{$entryobj->id}\"> " . PHP_EOL;
-        $output .= "<param name=\"allowFullScreen\" value=\"true\" />" . PHP_EOL;
-        $output .= "<param name=\"allowNetworking\" value=\"all\" />" . PHP_EOL;
-        $output .= "<param name=\"allowScriptAccess\" value=\"always\" />" . PHP_EOL;
-        $output .= "<param name=\"bgcolor\" value=\"#000000\" /> " . PHP_EOL;
-        $output .= "<param name=\"wmode\" value=\"opaque\" /> " . PHP_EOL;
-    
-        $output .= "<param name=\"movie\" value=\"{$host}/index.php/kwidget/wid/_{$entryobj->partnerId}";
-        $output .= "/uiconf_id/{$uiconf}/entry_id/{$entryobj->id}\" />" . PHP_EOL;
-    
-        $output .= "<a rel=\"media:thumbnail\" href=\"{$host}/width/120/height/90/bgcolor/000000/type/2\"></a>". PHP_EOL;
-        $output .= "<span property=\"dc:description\" content=\"{$entryobj->description}\"></span>" . PHP_EOL;
-        $output .= "<span property=\"media:title\" content=\"{$entryobj->name}\"></span>" . PHP_EOL;
-        $output .= "<span property=\"media:width\" content=\"{$entryobj->width}\"></span>" . PHP_EOL;
-        $output .= "<span property=\"media:height\" content=\"{$entryobj->height}\"></span>" . PHP_EOL;
-        $output .= "<span property=\"media:type\" content=\"application/x-shockwave-flash\"></span>" . PHP_EOL;
-        $output .= "<span property=\"media:duration\" content=\"{$entryobj->duration}\"></span>". PHP_EOL;
-        $output .= "</object>" . PHP_EOL;
+		
+		$output = "<iframe id=\"kaltura_player_{$uid}\" src=\"{$host}/p/{$entryobj->partnerId}/sp/{$entryobj->partnerId}00/embedIframeJs/uiconf_id/{$uiconf}/partner_id/{$entryobj->partnerId}?iframeembed=true&playerId=kaltura_player_{$uid}&entry_id={$entryobj->id}\" width=\"560\" height=\"395\" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow=\"autoplay *; fullscreen *; encrypted-media *\" frameborder=\"0\"></iframe>";
 
         return $output;
     }
